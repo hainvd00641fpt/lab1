11d7ce55bd4d55b3abebbe37e8cce508c4226090
hainvd00641fpt <your_email_address@example.com>
2018-10-27 21:23:36 +0700

    first commit

---
+++ Hai/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hai.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hai.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("")]
+        public IActionResult GetList()
+        {
+            return View();
+        }
+       
+        public IActionResult Create(string name, int price)
+        {
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete(int ID)
+        {
+            return new JsonResult(new Product()
+            {
+                id = ID
+                
+            });
+        }
+    }
+}
